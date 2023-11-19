d06ca46ce9c1204eaceb90c27a586d1e1dad6877
harshita-jaiswal <hj7@pdx.edu>
2023-11-19 13:24:12 -0800

    added navigation item in nav-panel

--- NBA-Stats-Dashboard/src/NavMenu.jsx
+++ NBA-Stats-Dashboard/src/NavMenu.jsx
@@ -1,8 +1,58 @@
 function NavMenu() {
   return (
-    <div className='majorSection navMenu'>
-      <p>The main navigation menu will go here</p>
-    </div>
+    <ul className="majorSection navMenu">
+      <li className="navItem">
+        <img
+          className="navItem__icon"
+          src="../src/assets/stats.png"
+          height="24px"
+          width="24px"
+          alt="Icon of Statistics"
+        />
+        <span>Player Stats</span>
+        <img
+          className="navItem__icon-arrow"
+          src="../src/assets/arrow-right.png"
+          height="15px"
+          width="15px"
+          alt="Icon of right arrow"
+        />
+      </li>
+      <li className="navItem">
+        <img
+          className="navItem__icon"
+          src="../src/assets/compare.png"
+          height="24px"
+          width="24px"
+          alt="Icon of Comparisons"
+        />
+        <span>Player Comparisons</span>
+        <img
+          className="navItem__icon-arrow"
+          src="../src/assets/arrow-right.png"
+          height="15px"
+          width="15px"
+          alt="Icon of right arrow"
+        />
+      </li>
+      <li className="navItem">
+        <img
+          className="navItem__icon"
+          src="../src/assets/geomap.png"
+          height="24px"
+          width="24px"
+          alt="Icon of GeoMaps"
+        />
+        <span>GeoMaps</span>
+        <img
+          className="navItem__icon-arrow"
+          src="../src/assets/arrow-right.png"
+          height="15px"
+          width="15px"
+          alt="Icon of right arrow"
+        />
+      </li>
+    </ul>
   );
 }
 
