4bb8a51a423077209dbf12436b26711ceeb007ee
harshita-jaiswal <hj7@pdx.edu>
2023-11-19 13:59:13 -0800

    added routing for the main content

--- NBA-Stats-Dashboard/src/components/MainContent.jsx
+++ NBA-Stats-Dashboard/src/components/MainContent.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import PlayerStats from './PlayerStats';
+import Comparison from './Comparison';
+import GeoMaps from "./GeoMaps";
+
 function MainContent() {
   return (
     <div className='majorSection mainContent'>
-      <h1>Main Content will go here</h1>
+      <Routes>
+        <Route path="/" element={<PlayerStats />}></Route>
+        <Route path="/compare" element={<Comparison />}></Route>
+        <Route path="/geo-maps" element={<GeoMaps />}></Route>
+      </Routes >
     </div>
   );
 }
