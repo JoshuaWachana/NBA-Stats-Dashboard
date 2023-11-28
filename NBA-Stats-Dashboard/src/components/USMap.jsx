a5ec637aa3ac6979872a14e85c05394b4290e5de
Joshua Wachana <jjwachana@gmail.com>
2023-11-28 02:01:15 -0800

    us map displaying on page, but no data

--- NBA-Stats-Dashboard/src/components/USMap.jsx
+++ NBA-Stats-Dashboard/src/components/USMap.jsx
@@ -1,5 +1,35 @@
+import { Chart } from 'react-google-charts';
+
 function USMap() {
-  return <div>US Map - Birthplaces</div>;
+  const data = [
+    ['State', 'Latitude'],
+    ['California', 200],
+    ['WA', 500],
+  ];
+
+  const options = {
+    region: 'US',
+    displayMode: 'regions',
+    resolution: 'provinces',
+    colorAxis: { colors: ['#00853f', 'black', '#e31b23'] },
+    backgroundColor: '#81d4fa',
+    datalessRegionColor: '#f8bbd0',
+    defaultColor: '#f5f5f5',
+  };
+  return (
+    <div>
+      <div>US Map - Birthplaces</div>{' '}
+      <div>
+        <Chart
+          chartType='GeoChart'
+          width='100%'
+          height='400px'
+          data={data}
+          options={options}
+        />
+      </div>{' '}
+    </div>
+  );
 }
 
 export default USMap;
