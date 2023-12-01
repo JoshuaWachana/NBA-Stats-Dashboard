0a79f03c078a196c1b1337fbdd88b2f81881a552
harshita-jaiswal <hj7@pdx.edu>
2023-12-01 11:27:43 -0800

    Added App.jsx test case

---
+++ NBA-Stats-Dashboard/src/__test__/App.test.jsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import App from '../App';
+
+jest.mock('../components/Header', () => () => <div data-testid="header-mock">Header</div>);
+jest.mock('../components/Footer', () => () => <div data-testid="footer-mock">Footer</div>);
+jest.mock('../components/NavMenu', () => () => <div data-testid="nav-menu-mock">NavMenu</div>);
+jest.mock('../components/MainContent', () => () => <div data-testid="main-content-mock">MainContent</div>);
+
+describe('App Component', () => {
+    it('should render all components correctly', () => {
+        const { getByTestId } = render(<App />);
+
+        expect(getByTestId('header-mock')).toBeInTheDocument();
+        expect(getByTestId('footer-mock')).toBeInTheDocument();
+        expect(getByTestId('nav-menu-mock')).toBeInTheDocument();
+        expect(getByTestId('main-content-mock')).toBeInTheDocument();
+    });
+});
